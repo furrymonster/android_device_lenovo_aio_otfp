369b79644845708a7567ec512c6ec16ad4e17c59
furrymonster <furrymonster@gmx.com>
2015-10-03 17:24:43 +0100

    Modified cm.mk to see if lunch works

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ PRODUCT_RELEASE_NAME := lemon
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lemon
-PRODUCT_NAME := lemon
+PRODUCT_NAME := cm_lemon
 PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := lemon
 PRODUCT_MANUFACTURER := lenovo
