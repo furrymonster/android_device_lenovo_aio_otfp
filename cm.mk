17ce3bcce7a9931a994dfa2cf64779421b3b24af
furrymonster <furrymonster@gmx.com>
2015-11-08 21:36:50 +0000

    Added inherit-product for common_full_phone.mk

--- cm.mk
+++ cm.mk
@@ -1,10 +1,11 @@
 # Release name
 PRODUCT_RELEASE_NAME := lemon
 
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lemon
 PRODUCT_NAME := cm_lemon
 PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := lemon
 PRODUCT_MANUFACTURER := lenovo
-PRODUCT_RELEASE_NAME := lemon
